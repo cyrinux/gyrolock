450a6415ecf51adaeb06b439069c104a11c1904c
Cyril Levis <git@levis.name>
2022-03-12 15:23:56 +0100

    chore: wip

--- go.mod
+++ go.mod
@@ -2,7 +2,6 @@ module github.com/cyrinux/gyrolock
 
 go 1.17
 
-require (
-	github.com/coreos/go-systemd v0.0.0-20191104093116-d3cd4ed1dbcf // indirect
-	github.com/godbus/dbus v4.1.0+incompatible // indirect
-)
+require github.com/coreos/go-systemd v0.0.0-20191104093116-d3cd4ed1dbcf
+
+require github.com/godbus/dbus v4.1.0+incompatible // indirect
--- main.go
+++ main.go
@@ -13,63 +13,92 @@ import (
 	"github.com/coreos/go-systemd/login1"
 )
 
-func main() {
+// Sensor is a iio accelerometer struct
+type Sensor struct {
+	InitX int64
+	InitY int64
+	InitZ int64
+	X     int64
+	Y     int64
+	Z     int64
+}
 
+func main() {
 	debug, _ := strconv.ParseBool(os.Getenv("DEBUG"))
 	sensitivity, err := strconv.ParseInt(os.Getenv("SENSITIVITY"), 0, 16)
-	if err != nil {
+	if err != nil || sensitivity < 0 || sensitivity > 200 {
 		sensitivity = 100
 	}
 	if debug {
 		log.Printf("SENSITIVITY: %d", sensitivity)
 	}
-
-	initX := ReadSensor("x")
-	initY := ReadSensor("y")
-	initZ := ReadSensor("z")
-
-	if debug {
-		log.Printf("x:%v y:%v z:%v\n", initX, initY, initZ)
-	}
-
+	s := New()
+	s.Calibrate(debug)
 	for {
-
-		x := ReadSensor("x")
-		y := ReadSensor("y")
-		z := ReadSensor("z")
-
-		if debug {
-			log.Printf("x:%v y:%v z:%v\n", x, y, z)
-		}
-
-		if (initX+(initX*sensitivity)/100) < x && (initY+(initY*sensitivity)/100) < y || (initZ+(initZ*sensitivity)/100) < z {
+		s.Get(debug)
+		if s.CheckShake(sensitivity) {
 			LockSession(debug)
-			initX = x
-			initY = y
-			initZ = z
 			time.Sleep(15 * time.Second)
+			s.Calibrate(debug)
 		} else {
-			time.Sleep(2 * time.Second)
+			time.Sleep(1 * time.Second)
 		}
 	}
+}
 
+// New create a new sensor
+func New() *Sensor {
+	s := Sensor{}
+	return &s
+}
+
+// Get the values of the sensor
+func (s *Sensor) Get(debug bool) {
+	s.X = ReadSensor(1, "x")
+	s.Y = ReadSensor(1, "y")
+	s.Z = ReadSensor(1, "z")
+	if debug {
+		log.Printf("x:%v y:%v z:%v\n", s.X, s.Y, s.Z)
+	}
+}
+
+// Calibrate the sensor
+func (s *Sensor) Calibrate(debug bool) {
+	s.InitX = ReadSensor(1, "x")
+	s.InitY = ReadSensor(1, "y")
+	s.InitZ = ReadSensor(1, "z")
+	if debug {
+		log.Printf("init: x:%v y:%v z:%v\n", s.InitX, s.InitY, s.InitZ)
+	}
+}
+
+// CheckShake check if sensor was shake
+func (s *Sensor) CheckShake(sensitivity int64) bool {
+	return (s.InitX+(s.InitX*sensitivity)/100) < s.X ||
+		(s.InitY+(s.InitY*sensitivity)/100) < s.Y || (s.InitZ+(s.InitZ*sensitivity)/100) < s.Z
 }
 
 // ReadSensor read sensor value as absolute value
-func ReadSensor(axis string) int64 {
+func ReadSensor(device int, axis string) int64 {
 	var value int64
+	var scale float64
 	for {
-		content, err := ioutil.ReadFile(fmt.Sprintf("/sys/bus/iio/devices/iio:device1/in_accel_%s_raw", axis))
+		content, err := ioutil.ReadFile(fmt.Sprintf("/sys/bus/iio/devices/iio:device%d/in_accel_%s_raw", device, axis))
 		if err != nil {
-			log.Fatalf("Can't read sensor %s\n", axis)
+			log.Fatalf("Can't read sensor %s", axis)
 		}
 		value, _ = strconv.ParseInt(strings.TrimSpace(string(content)), 0, 64)
+		content, err = ioutil.ReadFile(fmt.Sprintf("/sys/bus/iio/devices/iio:device%d/in_accel_scale", device))
+		if err != nil {
+			log.Fatalf("Can't read sensor")
+		}
+		scale, _ = strconv.ParseFloat(strings.TrimSpace(string(content)), 64)
 		if value != 0 {
 			break
 		}
-		time.Sleep(500 * time.Millisecond)
+		time.Sleep(200 * time.Millisecond)
 	}
-	return int64(math.Abs(float64(value)))
+	return int64(math.Abs(float64(value)) * scale)
 }
 
 // LockSession lock the current session
